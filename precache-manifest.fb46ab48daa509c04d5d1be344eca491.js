dd74bc6235b8731eb41d4e93210818259748b551
Ranjan Nayak <swadhin.ranjannayak@siemens.com>
2020-11-02 08:25:29 +0530

    Updates

---
+++ precache-manifest.fb46ab48daa509c04d5d1be344eca491.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "867a03b9908853666feafac7c1a2e71d",
+    "url": "/spacex-app/index.html"
+  },
+  {
+    "revision": "a0bc49cbe5eaed80dacd",
+    "url": "/spacex-app/static/css/main.9d5a1722.chunk.css"
+  },
+  {
+    "revision": "f9ab9980213d1d17c683",
+    "url": "/spacex-app/static/js/2.0d3aceeb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/spacex-app/static/js/2.0d3aceeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0bc49cbe5eaed80dacd",
+    "url": "/spacex-app/static/js/main.be73d139.chunk.js"
+  },
+  {
+    "revision": "43007cde8bbe4a6ee8a6",
+    "url": "/spacex-app/static/js/runtime-main.1fa3d291.js"
+  }
+]);
